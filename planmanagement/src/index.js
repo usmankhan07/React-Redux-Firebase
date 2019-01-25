6d79d5970057f62087994f587b16eeaf61708595
Usman Khan <muhammadusmankhan307@gmail.com>
2019-01-26 02:39:31 +0500

    Adding Redux, adding Store and Reducers in app.

--- planmanagement/src/index.js
+++ planmanagement/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+
+
+ReactDOM.render(<Provider store = {store}> <App /> </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
